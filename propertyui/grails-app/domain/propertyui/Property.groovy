be2bf5280e67f2304abfedfc31cb12f5ed839544
widi <widiyantoko@ymail.com>
2019-10-21 16:34:44 +0800

    Update enums

--- propertyui/grails-app/domain/propertyui/Property.groovy
+++ propertyui/grails-app/domain/propertyui/Property.groovy
@@ -1,7 +1,83 @@
 package propertyui
 
+import Enums.PropertyFeature
+import Enums.PropertyType
+import Enums.PropertyStatus
+
 class Property {
 
+    static belongsTo = [user: User]
+
+    PropertyFeature propertyFeature
+    PropertyType propertyType
+    PropertyStatus propertyStatus
+
+    String title
+    String gallery
+    String address
+    String city
+    String state
+    String postalCode
+    String detailInfo
+    String description
+
+    Integer totalRoom
+    Integer totalBathRoom
+    Integer totalBedRoom
+    Integer buildingAge
+    Integer stars
+
+    User createBy
+    User updateBy
+    User contactInfo
+    User reviewBy
+
+    BigDecimal price
+
+    Boolean isSale = Boolean.FALSE
+    Boolean isAvailable = Boolean.TRUE
+
+    Date createDate
+    Date lastModified
+    Date paidDate
+
     static constraints = {
+        propertyFeature nullable: true
+        propertyType nullable: true
+        propertyStatus nullable: true
+
+        title nullable: true
+        gallery nullable: true , blank: true
+        address nullable: true
+        city nullable: true
+        state nullable: true
+        postalCode nullable: true
+        detailInfo nullable: true
+        description nullable: true
+
+        totalRoom nullable: true
+        totalBathRoom nullable: true
+        totalBedRoom nullable: true
+        buildingAge nullable: true
+        stars nullable: true
+
+        createBy nullable: true
+        updateBy nullable: true
+        contactInfo nullable: true
+        reviewBy nullable: true
+
+        price nullable: true
+
+        isSale nullable: true
+        isAvailable nullable: true
+
+        createDate nullable: true
+        lastModified nullable: true
+        paidDate nullable: true
+    }
+
+    static mapping = {
+        description sqlType: "text"
+        gallery sqlType: "text"
     }
 }
--- propertyui/src/main/groovy/Enums/PropertyStatus.groovy
+++ propertyui/src/main/groovy/Enums/PropertyStatus.groovy
@@ -1,5 +1,5 @@
 package Enums
 
-enum SaleType {
+enum PropertyStatus {
     SALE, RENT
 }
